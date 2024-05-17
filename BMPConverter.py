f23abe1adcd8e580b2e73657030a8fbbb4a58a7b
star8kid <tonymstar5687@yahoo.com>
2024-05-16 22:23:47 -0400

    Write notes about main goals for the converter file. Research library methods from io and Image.
    Started the repository and wrote out a few goals for the function. Still need to refresh on python methods and syntax

---
+++ BMPConverter.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import io
+
+
+# Program Goals
+
+
+# Create a function that will write out a uint8_t array from a single .bmp file
+# Overload the function to write out an arry of uint8_t arrays from either several .bmp files or a folder than contains several .bmp files
+# Push the contents of said function to an output file that can be copy-pasted into a Source C++ file
