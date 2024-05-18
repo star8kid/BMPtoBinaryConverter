22bbfb20cc7eeb28e74e2db4649843a38bee1665
star8kid <tonymstar5687@yahoo.com>
2024-05-17 22:08:41 -0400

    Research more about the functions needed for looping through each row and column of pixels inside the BMP file. Next, figure out how to access each pixel and detect if it is white (0) or non-white (1)

--- BMPConverter.py
+++ BMPConverter.py
@@ -5,6 +5,48 @@ import io
 # Program Goals
 
 
-# Create a function that will write out a uint8_t array from a single .bmp file
+# Create a function that will write out a uint8_t array (with binary values) from a single .bmp file
 # Overload the function to write out an arry of uint8_t arrays from either several .bmp files or a folder than contains several .bmp files
 # Push the contents of said function to an output file that can be copy-pasted into a Source C++ file
+
+
+# Define Filepath Constants (Edit these to select which file to convert)
+
+inputPath = "Input/testFile.bmp"
+outputPath = "Output/testFileBinary.h"
+
+
+def BMPtoBinary(arrayName, file):
+    image = Image.open(file)
+    imageConverted1 = image.convert("L")
+    imageConverted1.show()
+    # imageConverted2 = image.convert("1")
+    # imageConverted2.show()
+
+    # Start writing out code to write out uint8_t binary
+    width, height = image.size
+    for a in range (height):
+        for b in range (width):
+
+            print("Filler Text!")
+    pass
+    # print(width)
+    # print(height)
+
+    return arrayName
+
+
+# BMPtoBinary("testFrames", inputPath)
+
+image = Image.open(r"./Input/testFile.bmp")
+imageConverted1 = image.convert("L")
+imageConverted1.show()
+
+
+
+
+
+# index = image.getpixel((5,0))
+# testString = str(index)
+# print(testString)
+
